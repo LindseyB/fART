0cfea4dd7b2056566f747a4ae74c846e546d3a33
Lindsey Bieda <hazardous.narayan@gmail.com>
2025-08-28 16:50:38 -0700

    Update dat str

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -110,8 +110,9 @@ class ArtGalleryLightbox {
     const fullSizeUrl = currentItem.href;
 
     // Update lightbox content
-    lightbox.querySelector('.art-lightbox-image').src = img.src.replace('&w=300&h=300', '&w=800&h=800');
-    lightbox.querySelector('.art-lightbox-image').src = img.src.replace('t=square', '');
+    const new_src = img.src.replace('&t=square', '');
+    new_src.replace('&q=50', '');
+    lightbox.querySelector('.art-lightbox-image').src = new_src.replace('&w=300&h=300', '&w=800&h=800');
     lightbox.querySelector('.art-lightbox-image').alt = img.alt;
     lightbox.querySelector('.art-lightbox-title').textContent = title.textContent;
     lightbox.querySelector('.art-lightbox-number').textContent = number.textContent;
