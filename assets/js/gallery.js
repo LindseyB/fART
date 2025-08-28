f5f58d104b374be809056db3d6aa1f81bf26f73a
Lindsey Bieda <hazardous.narayan@gmail.com>
2025-08-28 16:58:02 -0700

    It's not a const now is it lindsey

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -110,7 +110,7 @@ class ArtGalleryLightbox {
     const fullSizeUrl = currentItem.href;
 
     // Update lightbox content
-    const new_src = img.src.replace('&t=square', '');
+    let new_src = img.src.replace('&t=square', '');
     new_src = new_src.replace('&q=50', '');
     lightbox.querySelector('.art-lightbox-image').src = new_src.replace('&w=300&h=300', '&w=800&h=800');
     lightbox.querySelector('.art-lightbox-image').alt = img.alt;
