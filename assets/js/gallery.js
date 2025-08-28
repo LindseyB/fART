a345bacd5efc60c21658a0e8b1397ae0cf46a342
Lindsey Bieda <hazardous.narayan@gmail.com>
2025-08-28 16:45:04 -0700

    Remove the sqaure

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -111,6 +111,7 @@ class ArtGalleryLightbox {
 
     // Update lightbox content
     lightbox.querySelector('.art-lightbox-image').src = img.src.replace('&w=300&h=300', '&w=800&h=800');
+    lightbox.querySelector('.art-lightbox-image').src = img.src.replace('t=square', '');
     lightbox.querySelector('.art-lightbox-image').alt = img.alt;
     lightbox.querySelector('.art-lightbox-title').textContent = title.textContent;
     lightbox.querySelector('.art-lightbox-number').textContent = number.textContent;
